d0920949336fbeb8f3e603b648f7f30cb27076b9
gurleen-kaurr <gurleen.kaurrrrrr@gmail.com>
2024-02-05 20:46:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function main(){
+async function getSongs(){
     
-    let a =  await fetch("")
+    let a =  await fetch("C:\Users\hp\Documents\GitHub\SpotifyClone\script.js")
     let response = await a.text();
-    console.log(response)   
+    console.log(response) 
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for(let index = 0; index< as.length; index++){
+        const div = as[index];
+        if(div.href.endsWith(".mp3")){
+            songs.push(div.href)
+        }
+    }
+    return songs
 }
 
+getSongs()
